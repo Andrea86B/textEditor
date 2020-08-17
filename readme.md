264ae3201d465b3ca1c5ee11379621fc9f0219a8
Andrea Barba <andreabarba1986@gmail.com>
2020-08-17 16:55:23 +0200

    prova

---
+++ readme.md
@@ -0,0 +1,2 @@
+<h1>Text Editor</h1>
+to try this Text Editor click <a href="https://texteditorbarba.netlify.app/">HERE</a>
---
+++ script.js
@@ -0,0 +1,34 @@
+function updateText(){
+    let text = document.getElementById('text-input').value;
+    document.getElementById('text-output').innerText = text;
+  }
+
+function makeBold(elem){
+  elem.classList.toggle('active');
+  document.getElementById('text-output').classList.toggle('bold');
+}
+
+
+function makeItalic(elem){
+  elem.classList.toggle('active');
+  document.getElementById('text-output').classList.toggle('italic');
+}
+
+function makeUnderline(elem){
+  elem.classList.toggle('active');
+  if (document.getElementById('text-output').classList.contains('underline')){
+    document.getElementById('text-output').classList.remove('underline');
+  } else {
+    document.getElementById('text-output').classList.add('underline');
+  }
+}
+
+function alignText(elem, alignType){
+  let alignBtns = document.getElementsByClassName('align');
+  for(let i = 0; i < alignBtns.length; i++){
+      alignBtns[i].classList.remove('active');
+  }
+  elem.classList.toggle('active');
+  document.getElementById('text-output').style.textAlign = alignType;
+  
+}
